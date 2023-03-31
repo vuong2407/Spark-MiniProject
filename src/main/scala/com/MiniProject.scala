efb2ee4dd63e57875d6a4190f70544a5ef5f54a1
nathluu <luuhoangtanbis@gmail.com>
2023-03-31 15:26:50 +0700

    Change to abfs

--- src/main/scala/com/MiniProject.scala
+++ src/main/scala/com/MiniProject.scala
@@ -14,8 +14,8 @@ object MiniProject {
     val storageKeyValue = "VoSkdNrypk5juIgXQt6VkGcmUD+u+K5Q8fvwHmhHvUXHKksvh5+3Dpxk8AF8L4wrrqv8D0RaaKT0+AStIqooqA=="
 
     // Construct the full path to the CSV file in blob storage
-    val blobPath = s"wasbs://${containerName}@${storageAccountName}.blob.core.windows.net/${blobName}"
-//    val blobPath = s"abfss://${containerName}@${storageAccountName}.blob.core.windows.net/${blobName}"
+//    val blobPath = s"wasbs://${containerName}@${storageAccountName}.blob.core.windows.net/${blobName}"
+    val blobPath = s"abfss://${containerName}@${storageAccountName}.dfs.core.windows.net/${blobName}"
 
 
 
@@ -38,7 +38,9 @@ object MiniProject {
 
     // config for haddoop
     spark.conf.set("fs.azure.account.auth.type", "SharedKey")
-    spark.conf.set(s"fs.azure.account.key.${storageAccountName}.blob.core.windows.net",  storageKeyValue)
+    spark.conf.set(s"fs.azure.account.key.${storageAccountName}.dfs.core.windows.net",  storageKeyValue)
+//    spark.conf.set("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+//    spark.conf.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
 //    spark.sparkContext.hadoopConfiguration.set("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
 //    spark.sparkContext.hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
 //    spark.sparkContext.hadoopConfiguration.set(s"fs.azure.account.key.${storageAccountName}.blob.core.windows.net", storageKeyValue)
