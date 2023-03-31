1d38098c18a62fe907aeda750a1df7df81ec7539
vuong2407 <vanvuong24072001@gmal.com>
2023-03-31 12:31:39 +0700

    first-commit

---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := "Demo"
+
+version := "1.8.2"
+
+scalaVersion := "2.13.10"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+
+Compile / unmanagedJars := Seq.empty[sbt.Attributed[java.io.File]]
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.3.2",
+  "org.apache.spark" %% "spark-sql" % "3.3.2",
+  "org.apache.hadoop" % "hadoop-azure" % "3.3.5",
+  "org.apache.hadoop" % "hadoop-common" % "3.3.5",
+  "org.apache.hadoop" % "hadoop-core" % "1.2.1",
+  "com.microsoft.sqlserver" % "mssql-jdbc" % "7.2.1.jre8",
+)
---
+++ src/main/scala/com/MiniProject.scala
@@ -0,0 +1,58 @@
+package com
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import java.util.Properties
+
+object MiniProject {
+  def main(args: Array[String]): Unit = {
+
+    // Define variables for blob storage account and credentials
+    val storageAccountName = "vuongnguyen"
+    val containerName = "nifi-data"
+    val blobName = "mnm_dataset.csv"
+    val storageKeyValue = "VoSkdNrypk5juIgXQt6VkGcmUD+u+K5Q8fvwHmhHvUXHKksvh5+3Dpxk8AF8L4wrrqv8D0RaaKT0+AStIqooqA=="
+  
+    // Construct the full path to the CSV file in blob storage
+    val blobPath = s"wasbs://${containerName}@${storageAccountName}.blob.core.windows.net/${blobName}"
+
+    // Define variables for SQL Server connection
+    val jdbcUsername = "vuongnguyen"
+    val jdbcPassword = "Tma@2022@T1P"
+
+    // Construct the JDBC URL for SQL Server
+    val jdbcUrl = s"jdbc:sqlserver://vuongnguyen.database.windows.net:1433;database=SqlDatabase;user=vuongnguyen@vuongnguyen;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+    val connectionProperties = new Properties()
+    connectionProperties.put("user", s"${jdbcUsername}")
+    connectionProperties.put("password", s"${jdbcPassword}")
+    connectionProperties.put("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+
+    // Create a SparkSession
+    val spark = SparkSession.builder()
+      .appName("Read CSV from Blob Storage")
+      .master("local[*]")
+      .getOrCreate()
+
+    // config for haddoop
+    spark.sparkContext.hadoopConfiguration.set("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+    spark.sparkContext.hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+    spark.sparkContext.hadoopConfiguration.set(s"fs.azure.account.key.${storageAccountName}.blob.core.windows.net", storageKeyValue)
+
+    // Read the CSV file from blob storage into a DataFrame
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(blobPath)
+
+    // Group the DataFrame by state and color, and compute the count of total for each group
+    val groupCountMnMDF = df.groupBy("State", "Color")
+        .agg(count("Count").alias("total"))
+        .withColumnRenamed("State", "state")
+        .withColumnRenamed("Color", "color")
+
+    // Write the grouped DataFrame to SQL Server
+    groupCountMnMDF.write.jdbc(jdbcUrl, "warehouse.MnM", connectionProperties)
+
+  }
+
+}
